cb155cde8fb7924f56467c10467effbfe3d27607
Thais Menezes <thais.mas@hotmail.com>
2024-12-02 14:12:31 -0300

    learning about State

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,28 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
     var body: some View {
-        NavigationStack{
-            Form{
-                Section{
-                    Text("Hello World!")
-                }
-                Section{
-                    Text("Hello World!")
-                    Text("Hello World!")
-                    Text("Hello World!")
-                }
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
     }
     
