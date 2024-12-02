75006256925d3ed0d7d14bd2430b83649ad23e87
Thais Menezes <thais.mas@hotmail.com>
2024-12-02 14:05:55 -0300

    creating navigation stack with title

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,25 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form{
-            Section{
+        NavigationStack{
+            Form{
+                Section{
+                    Text("Hello World!")
+                }
+                Section{
+                    Text("Hello World!")
+                    Text("Hello World!")
+                    Text("Hello World!")
+                }
+                Text("Hello World!")
+                Text("Hello World!")
+                Text("Hello World!")
+                Text("Hello World!")
+                Text("Hello World!")
+                Text("Hello World!")
+                Text("Hello World!")
                 Text("Hello World!")
             }
-            Section{
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
-            }
-            Text("Hello World!")
-            Text("Hello World!")
-            Text("Hello World!")
-            Text("Hello World!")
-            Text("Hello World!")
-            Text("Hello World!")
-            Text("Hello World!")
-            Text("Hello World!")
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
+    
 }
 
 #Preview {
