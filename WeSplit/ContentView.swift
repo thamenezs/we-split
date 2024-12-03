d9efacc85963686da52139dac4d2761ab117179f
Thais Menezes <thais.mas@hotmail.com>
2024-12-02 21:06:36 -0300

    Blinding states to user control

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
+    @State private var name = ""
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
+        Form {
+            TextField("Enter your name:", text: $name)
+            Text("Your name is \(name)")
         }
     }
     
